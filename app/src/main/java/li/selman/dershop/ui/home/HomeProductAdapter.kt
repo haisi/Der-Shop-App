1a9c5e6f48ec4bf75aec63dede8509ade8aa70ea
Hasan Kara <hasan.selman.kara@gmail.com>
2020-03-23 17:15:01 +0100

    [Refactor] Allow two ViewHolders

--- app/src/main/java/li/selman/dershop/ui/home/HomeProductAdapter.kt
+++ app/src/main/java/li/selman/dershop/ui/home/HomeProductAdapter.kt
@@ -11,8 +11,26 @@ import li.selman.dershop.R
 class HomeProductAdapter(private val products: List<String> = emptyList()) :
     Adapter<ViewHolder>() {
 
+    enum class ProductTypes {
+        A, B
+    }
+
+    interface Inflatable {
+        fun inflate(parent: ViewGroup) : ViewHolder
+    }
+
     class ProductViewHolder(itemView: View) : ViewHolder(itemView) {
         private val discountText: TextView = itemView.findViewById(R.id.discount_percent)
+
+        companion object : Inflatable {
+            override fun inflate(parent: ViewGroup): ViewHolder {
+                val view = LayoutInflater
+                    .from(parent.context)
+                    .inflate(R.layout.product_summary_view, parent, false)
+
+                return ProductViewHolder(view)
+            }
+        }
 
         fun bind(item: String) {
             // If we need access to resources
@@ -22,12 +40,26 @@ class HomeProductAdapter(private val products: List<String> = emptyList()) :
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.product_summary_view, parent, false)
+    override fun getItemViewType(position: Int): Int {
+        val item = products[position]
 
-        return ProductViewHolder(view)
+        // TODO item should be a real class and make class check
+        return if (item.startsWith("A")) {
+            ProductTypes.A.ordinal
+        } else {
+            ProductTypes.B.ordinal
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return when (viewType) {
+            ProductTypes.A.ordinal -> {
+                ProductViewHolder.inflate(parent)
+            }
+            else -> {
+                ProductViewHolder.inflate(parent)
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -40,7 +72,6 @@ class HomeProductAdapter(private val products: List<String> = emptyList()) :
                 throw IllegalStateException("Unknown ViewHolder of type ${holder.javaClass}")
             }
         }
-
     }
 
     override fun getItemCount(): Int = products.size
