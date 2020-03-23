d80c241cc1d06afd91f2a966665d504b33aa295c
Hasan Kara <hasan.selman.kara@gmail.com>
2020-03-23 16:17:12 +0100

    Add custom list item layout

--- app/src/main/java/li/selman/dershop/ui/home/HomeProductAdapter.kt
+++ app/src/main/java/li/selman/dershop/ui/home/HomeProductAdapter.kt
@@ -1,28 +1,31 @@
 package li.selman.dershop.ui.home
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.product_summary_view.view.*
+import li.selman.dershop.R
 
 class HomeProductAdapter(private val products: List<String> = emptyList()) :
     RecyclerView.Adapter<HomeProductAdapter.ProductViewHolder>() {
 
-    class ProductViewHolder(val name: TextView) : RecyclerView.ViewHolder(name)
+    class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val discountText: TextView = itemView.findViewById(R.id.discount_percent)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        // TODO inflate View instead like:
-        /*
-            val textView = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.my_text_view, parent, false) as TextView
-         */
-        val textView = TextView(parent.context)
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.product_summary_view, parent, false)
 
-        return ProductViewHolder(textView)
+        return ProductViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         val item = products[position]
-        holder.name.text = item
+        holder.discountText.text = item
     }
 
     override fun getItemCount(): Int = products.size
