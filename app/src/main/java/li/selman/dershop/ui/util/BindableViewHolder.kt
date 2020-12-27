01daef0959c9fa6b4ac851289830f24e2c4dea16
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2020-12-27 19:47:16 +0100

    Refactor: remove unused code

--- app/src/main/java/li/selman/dershop/ui/util/BindableViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package li.selman.dershop.ui.util
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-abstract class BindableViewHolder<E>(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    abstract fun bind(item: E, listener: Any? = null)
-}
--- app/src/main/java/li/selman/dershop/ui/util/Inflatable.kt
+++
@@ -1,7 +0,0 @@
-package li.selman.dershop.ui.util
-
-import android.view.ViewGroup
-
-interface Inflatable<E> {
-    fun inflate(parent: ViewGroup): BindableViewHolder<E>
-}
